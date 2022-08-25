e0d8c760a28c30480c482979a6e803f540af2099
Taha Muslu <taha-_muslu1216@hotmail.com>
2022-08-25 08:22:47 +0300

    make it better

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/about", pageControllers.getAboutPage);
 app.get("/add_photo", pageControllers.getAddPage);
 app.get("/photos/edit/:id", pageControllers.getEditPage);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
